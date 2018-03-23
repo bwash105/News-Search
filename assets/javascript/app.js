fa1cb1cb7e68461b7a3b217eeec6d1135e8f0a60
Michael Johnson <michaeljohnson@Michaels-MacBook-Pro.local>
2018-03-23 14:12:34 -0700

    latest changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,11 +5,53 @@ var searchTerm;
 var numberRecords;
 var startYear;
 var endYear;
-$("submit-search").on("click", function(){
+$("#submit-search").on("click", function(){
   searchTerm = $("#search").val();
   numberRecords = $("#records").val();
   startYear = $("#start-year").val();
   endYear = $("#end-year").val();
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).then(function(result) {
+    console.log(result);
+    var articles = result.response.docs
+    
+    for (var i = 0; i < numberRecords; i++) {
+      var webUrl = articles[i].web_url;
+      var headline = articles[i].headline.main;
+      var byLine = articles[i].byline.original;
+      var pubDate = articles[i].pub_date;
+      console.log(webUrl);
+      console.log(headline);
+      console.log(byLine);
+      console.log(pubDate);
+      var articleDiv = $("<div>");
+  
+      var number = $("<div>" + (i+1) + "</div>");
+      articleDiv.append(number);
+  
+      var title = $("<h1>");
+      title.text(headline);
+      articleDiv.append(title);
+  
+      var author = $("<p>");
+      author.text(byLine);
+      articleDiv.append(author);
+  
+      var date = $("<p>");
+      date.text(pubDate);
+      articleDiv.append(date);
+  
+      var link = $("<a>");
+      link.text(webUrl);
+      link.attr("href", webUrl);
+      articleDiv.append(link);
+      $("#top-articles").append(articleDiv);
+    }
+  }).fail(function(err) {
+    throw err;
+  });
 });
 $("#clear-results").on("click",function(){
   $("#top-articles").empty();
@@ -25,45 +67,3 @@ if (endYear !== ""){
 
 }
 
-$.ajax({
-  url: url,
-  method: 'GET',
-}).then(function(result) {
-  console.log(result);
-  var articles = result.response.docs
-  
-  for (var i = 0; i < numberRecords; i++) {
-    var webUrl = articles[i].web_url;
-    var headline = articles[i].headline.main;
-    var byLine = articles[i].byline.original;
-    var pubDate = articles[i].pub_date;
-    console.log(webUrl);
-    console.log(headline);
-    console.log(byLine);
-    console.log(pubDate);
-    var articleDiv = $("<div>");
-
-    var number = $("<div>" + (i+1) + "</div>");
-    articleDiv.append(number);
-
-    var title = $("<h1>");
-    title.text(headline);
-    articleDiv.append(title);
-
-    var author = $("<p>");
-    author.text(byLine);
-    articleDiv.append(author);
-
-    var date = $("<p>");
-    date.text(pubDate);
-    articleDiv.append(date);
-
-    var link = $("<a>");
-    link.text(webUrl);
-    link.attr("href", webUrl);
-    articleDiv.append(link);
-    $("#top-articles").append(articleDiv);
-  }
-}).fail(function(err) {
-  throw err;
-});
